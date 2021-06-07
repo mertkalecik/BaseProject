b0efd9fa61311f8882571948446fdef3440eb57f
mertkalecik <mert.kalecik@hotmail.com>
2021-06-08 01:04:51 +0300

    id_added_list_place_holder

--- app/src/main/java/com/base/mert/ing/mapper/Mapper.kt
+++ app/src/main/java/com/base/mert/ing/mapper/Mapper.kt
@@ -4,4 +4,4 @@ import com.base.mert.ing.ui.data.home.Person
 import com.base.mert.ing.ui.data.home.RepoEntity
 
 fun Person.toRepoEntity() =
-        RepoEntity(id = id, name = fullName)
+        RepoEntity(id = id, name = "$fullName ($id)")
